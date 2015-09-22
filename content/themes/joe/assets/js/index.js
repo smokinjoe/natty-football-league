1ce77dd58a1458284c879145f9e71602617b9827
Joe <joe.ekiert@gmail.com>
2015-09-22 04:45:58 +0000

    whole slew of features and updates, ecma6, classes instead, new syntax, based off of the theme develop branch

--- content/themes/joe/assets/js/index.js
+++ content/themes/joe/assets/js/index.js
@@ -4,39 +4,32 @@
 
 /* globals jQuery, document */
 (function ($, undefined) {
-    "use strict";
+  "use strict";
 
-  var Ajax = (function (opts) {
-    var opts = opts || {},
-        params = opts.params || {},
+  const TRIGGER_CLICK = 'click',
+      TRIGGER_HOVER = 'hover',
+      TRIGGER_AUTO = 'auto',
+      TRIGGER_DEFAULT = TRIGGER_CLICK;
+
+  class Ajax {
+    static call (opts) {
+      opts = opts || {};
+      let params = opts.params || {},
         callback = opts.callback || new Function(),
         errCb = opts.err || new Function(),
         url = opts.url,
-        method = opts.method || 'GET',
-        methods = {};
+        method = opts.method || opts.type || 'GET';
 
-    methods.call = function (opts) {
       $.ajax({
         url: url,
         type: method,
         data: params
       }).done(callback).fail(errCb);
-    };
+    }
 
-
-        /*
-          var parser = document.createElement('a');
-          parser.href = "http://example.com:3000/pathname/?search=test#hash";
-          parser.protocol; // => "http:"
-          parser.hostname; // => "example.com"
-          parser.port;     // => "3000"
-          parser.pathname; // => "/pathname/"
-          parser.search;   // => "?search=test"
-          parser.hash;     // => "#hash"
-          parser.host;     // => "example.com:3000"
-        */
-    methods.parser = function (url) {
-      var parser = document.createElement('a'),
+    static parser (url) {
+      url = url || this.url || '';
+      let parser = document.createElement('a'),
           structure = {};
       parser.href = url;
 
@@ -51,12 +44,286 @@
       };
 
       return structure;
+    }
+  };
+
+  class u {
+    static rest (arr) {
+      var clone = this.clone(arr, []);
+      clone.splice(0, 1);
+      return clone;
+    };
+    static clone (thing, extender) {
+      extender = extender || {};
+      return jQuery.extend(true, extender, thing);
+    };
+  };
+
+  class J {
+    constructor ($element, options) {
+      if ($.isEmptyObject(options)) {
+
+        if ($element.data('inline')) {
+          options = $element.data('inline');
+        }
+        else {
+          var args = $element.text().split(',');
+          options = args[1] ? JSON.parse(u.rest(args).join(',')) || {} : {};
+          options.body = args[0];
+        }
+
+      }
+
+      options.$element =      $element || $("<div />");
+      options.body =          options.body || $element.text();
+      options.src =           options.src || $element.data('src') || $element.attr('href');
+      options.autoplay =      options.autoplay || false;
+      options.trigger =       options.trigger || TRIGGER_DEFAULT;
+
+      this.config = options;
+      this.$element = options.$element;
     };
 
-    return methods;
-  }());
+    generate () {
+      let config = this.config;
 
-  var $document = $(document);
+      if (config.src.match(/streamable/)) {
+        this.handleStreamableMedia(config);
+      }
+      else if (config.src.match(/mp4/)) {
+        this.handleVidFile(config);
+      }
+      else if (config.src.match(/gfycat/)) {
+        this.handleGfyCat(config);
+      }
+      else if (config.src.match(/gifv/)) {
+        this.handleImgur(config);
+      }
+      else if (config.src.match(/gif/)) {
+        this.buildIMGWith(config);
+      }
+      else if (config.src.match(/jpg|jpeg|png/)) {
+        this.buildIMGWith(config);
+      }
+      else if (config.src.match(/youtube/)) {
+        this.handleYouTube(config);
+      }
+      else {
+        console.log("JOE: Link doesn't have fun media.");
+      }
+    }
+
+    // Various handlers
+    handleStreamableMedia () {
+      let uriArray = this.config.src.split('/'),
+          streamableId = uriArray[uriArray.length - 1],
+          src = '//streamable.com/res/' + streamableId,
+          $element = this.$element;
+
+      this.buildiFrame({
+        $element: $element,
+        width: '100%',
+        src: src,
+        scrolling: 'no'
+      });
+    };
+
+    handleGfyCat () {
+      let uriArray = this.config.src.split('/'),
+          gfyCatDealie = uriArray[uriArray.length - 1],
+          $element = this.$element,
+          that = this,
+          webmSrc, mp4Src;
+
+      Ajax.call({
+        url: '//gfycat.com/cajax/get/' + gfyCatDealie,
+        method: 'GET',
+        callback: function (response) {
+          webmSrc = response.gfyItem.webmUrl;
+          mp4Src = response.gfyItem.mp4Url;
+          that.buildHTML5Video({
+            $element: $element,
+            wrapChildSources: true,
+            webmSrc: webmSrc,
+            mp4Src: mp4Src
+          });
+        },
+        errCb: function () {
+          console.log("Error: Failure to retrieve response for gfycat: ", gfyCatDealie);
+        }
+      });
+
+    };
+
+    handleYouTube () {
+      let src = this.config.src,
+          result = Ajax.parser(src),
+          ytKey = result.search.split('=')[1],
+          $element = this.$element;
+
+      this.buildiFrame({
+        $element: $element,
+        src: '//youtube.com/embed/' + ytKey
+      });
+    };
+
+    handleImgur (opts) {
+      let $element = this.config.$element,
+          arr = $element.data('src').split('.');
+      arr.pop();
+      let src = arr.join('.'),
+          webmSrc = src + '.webm',
+          mp4Src = src + '.mp4';
+
+      this.buildHTML5Video({
+        $element: $element,
+        wrapChildSources: true,
+        webmSrc: webmSrc,
+        mp4Src: mp4Src
+      });
+
+    };
+
+    handleVidFile () {
+      this.buildHTML5Video(this.config);
+    };
+
+    // f()s that build html elments
+    buildHTML5Video (opts) {
+      opts = opts || {};
+      let height = opts.height || 450,
+          width = opts.width || '100%',
+          $element = opts.$element || $('<div />'),
+          src = opts.src || $element.data('src'),
+          autoplay = opts.autoplay,
+          //webmSrc = opts.webmSrc || (src + '.webm'),
+          //mp4Src = opts.mp4Src || (src + '.mp4'),
+          webmSrc = opts.webmSrc || false,
+          mp4Src = opts.mp4Src || false,
+          wrapChildSources = opts.wrapChildSources || false,
+          $video = $('<video>', {
+            height: height,
+            width: width,
+            loop: '',
+            autoplay: autoplay,
+            controls: '',
+            muted: 'muted'
+          }),
+          webmID = opts.webmID || 'webmsource',
+          mp4ID = opts.mp4ID || 'mp4source',
+          $webmSource, $mp4Source;
+
+      if (wrapChildSources) {
+        if (webmSrc) {
+          $webmSource = $('<source>', {
+            id: webmID,
+            src: webmSrc,
+            type: 'video/webm'
+          });
+          $video.append($webmSource);
+        }
+        if (mp4Src) {
+          $mp4Source = $('<source>', {
+            id: mp4ID,
+            src: mp4Src,
+            type: 'video/mp4'
+          });
+          $video.append($mp4Source);
+        }
+      }
+      else {
+        $video.attr('src', src);
+      }
+
+      this.finishItUp($element, $video, this.config);
+      return $element;
+    };
+
+    buildIMGWith ($element) {
+      let $img = $('<img>', {
+            src: $element.data('src'),
+            width: "100%"
+          });
+
+      this.finishItUp($element, $img, this.config);
+    };
+
+    buildiFrame (opts) {
+      opts = opts || {}
+      let height = opts.height || 450,
+          width = opts.width || 710,
+          scrolling = opts.scrolling || 'no',
+          $element = opts.$element || $('<div />'),
+          src = opts.src || $element.data('src'),
+          $iframe = $('<iframe>', {
+            src: src,
+            frameboarder: 0,
+            height: height,
+            width: width,
+            scrolling: scrolling,
+            allowfullscreen: ''
+          });
+
+      this.finishItUp($element, $iframe, this.config);
+
+      return $element;
+    };
+
+    finishItUp ($element, $mediaElement, opts) {
+      let trigger = opts.trigger;
+      switch (opts.trigger) {
+        case TRIGGER_CLICK:
+          this.handleClick($element, $mediaElement);
+        break;
+        case TRIGGER_HOVER:
+          this.handleHover($element, $mediaElement);
+        break;
+        case TRIGGER_AUTO:
+          this.showMedia($element, $mediaElement);
+        break;
+      }
+
+      if (opts.body) {
+        $element.text(opts.body);
+      }
+    };
+
+    // event handlers
+    handleClick ($trigger, $ammo) {
+      $trigger.on('click', e => {
+        e.preventDefault();
+
+        if ($ammo.is(':visible')) {
+          $ammo.detach();
+        }
+        else {
+          $ammo.appendTo($trigger);
+        }
+      });
+    };
+
+    handleHover ($trigger, $ammo) {
+      $trigger.on('click', e => e.preventDefault());
+      $trigger.on('mouseenter', e => {
+        if (!$ammo.is(':visible')) {
+          $ammo.appendTo($trigger);
+        }
+      });
+      $trigger.on('mouseleave', e => {
+        if ($ammo.is(':visible')) {
+          $ammo.detach();
+        }
+      });
+    };
+
+    showMedia ($trigger, $ammo) {
+      $ammo.appendTo($trigger);
+    };
+
+  };
+
+  var $document = $(document),
+      dealieArray = [];
 
   $document.ready(function () {
     checkInlineMedia();
@@ -64,193 +331,26 @@
   });
 
   var checkInlineMedia = function () {
-    $('a[data-inline]').each(function () {
-      var $this = $(this),
-          src = $this.data('src');
+    // $('a[data-inline]').each(function () {
+    //   var $this = $(this),
+    //       src = $this.data('src'),
+    //       dealie = new J({
+    //         $element: $this
+    //       });
 
-      if (src.match(/streamable/)) {
-        handleStreamableMedia($this);
-      }
-      else if (src.match(/mp4/)) {
-        handleVidFile($this);
-      }
-      else if (src.match(/gfycat/)) {
-        handleGfyCat($this);
-      }
-      else if (src.match(/gifv/)) {
-        handleImgur($this);
-      }
-      else if (src.match(/gif/)) {
-        buildIMGWith($this);
-      }
-      else if (src.match(/jpg|jpeg|png/)) {
-        buildIMGWith($this);
-      }
-      else if (src.match(/youtube/)) {
-        handleYouTube($this);
-      }
+    //   dealieArray.push(dealie);
+    // });
+
+    $('a').each(function () {
+      let $this = $(this),
+          dealie = new J($this, {});
+
+      dealie.generate();
+      dealieArray.push(dealie);
     });
   };
 
-  var handleStreamableMedia = function ($element) {
-    var uriArray = $element.data('src').split('/'),
-        streamableId = uriArray[uriArray.length - 1],
-        src = '//streamable.com/res/' + streamableId;
 
-    buildiFrame({
-      $element: $element,
-      width: '100%',
-      src: src,
-      scrolling: 'no'
-    });
-  };
-
-  var handleGfyCat = function ($element) {
-    var uriArray = $element.data('src').split('/'),
-        gfyCatDealie = uriArray[uriArray.length - 1],
-        webmSrc, mp4Src;
-
-    $.ajax({
-      url: '//gfycat.com/cajax/get/' + gfyCatDealie,
-      type: 'GET'
-    }).done(function (response) {
-      webmSrc = response.gfyItem.webmUrl;
-      mp4Src = response.gfyItem.mp4Url;
-      buildHTML5Video({
-        $element: $element,
-        wrapChildSources: true,
-        webmSrc: webmSrc,
-        mp4Src: mp4Src
-      });
-    }).fail(function () {
-      console.log("Error: Failure to retrieve response for gfycat: ", gfyCatDealie);
-    });;
-  };
-
-  var handleYouTube = function ($element) {
-    var src = $element.data('src'),
-        result = Ajax.parser(src),
-        ytKey = result.search.split('=')[1];
-
-    buildiFrame({
-      $element: $element,
-      src: '//youtube.com/embed/' + ytKey
-    });
-  }
-
-  var handleImgur = function ($element) {
-    var arr = $element.data('src').split('.');
-    arr.pop();
-    var src = arr.join('.'),
-        webmSrc = src + '.webm',
-        mp4Src = src + '.mp4';
-
-    buildHTML5Video({
-      $element: $element,
-      wrapChildSources: true,
-      webmSrc: webmSrc,
-      mp4Src: mp4Src
-    });
-
-  };
-
-  var handleVidFile = function ($element) {
-    buildHTML5Video({
-      $element: $element
-    });
-  };
-
-  var buildHTML5Video = function (opts) {
-    opts = opts || {};
-    var height = opts.height || 450,
-        width = opts.width || '100%',
-        $element = opts.$element || $('<div />'),
-        src = opts.src || $element.data('src'),
-        //webmSrc = opts.webmSrc || (src + '.webm'),
-        //mp4Src = opts.mp4Src || (src + '.mp4'),
-        webmSrc = opts.webmSrc || false,
-        mp4Src = opts.mp4Src || false,
-        wrapChildSources = opts.wrapChildSources || false,
-        $video = $('<video>', {
-          height: height,
-          width: width,
-          loop: '',
-          //autoplay: '',
-          controls: '',
-          muted: 'muted'
-        }),
-        webmID = opts.webmID || 'webmsource',
-        mp4ID = opts.mp4ID || 'mp4source',
-        $webmSource, $mp4Source;
-
-    if (wrapChildSources) {
-      if (webmSrc) {
-        $webmSource = $('<source>', {
-          id: webmID,
-          src: webmSrc,
-          type: 'video/webm'
-        });
-        $video.append($webmSource);
-      }
-      if (mp4Src) {
-        $mp4Source = $('<source>', {
-          id: mp4ID,
-          src: mp4Src,
-          type: 'video/mp4'
-        });
-        $video.append($mp4Source);
-      }
-    }
-    else {
-      $video.attr('src', src);
-    }
-
-    handleClick($element, $video);
-    return $element;
-  };
-
-  var buildIMGWith = function ($element) {
-    var $img = $('<img>', {
-          src: $element.data('src'),
-          width: "100%"
-        });
-
-    handleClick($element, $img);
-  };
-
-  var buildiFrame = function (opts) {
-    opts = opts || {}
-    var height = opts.height || 450,
-        width = opts.width || 710,
-        scrolling = opts.scrolling || 'no',
-        $element = opts.$element || $('<div />'),
-        src = opts.src || $element.data('src'),
-        $iframe = $('<iframe>', {
-          src: src,
-          frameboarder: 0,
-          height: height,
-          width: width,
-          scrolling: scrolling,
-          allowfullscreen: ''
-        });
-
-    handleClick($element, $iframe);
-
-    return $element;
-  }
-
-  var handleClick = function ($trigger, $ammo) {
-    $trigger.on('click', function (e) {
-      e.preventDefault();
-
-      if ($ammo.is(':visible')) {
-        $ammo.detach();
-      }
-      else {
-        $ammo.appendTo($trigger);
-      }
-    });
-  }
 
   // Arctic Scroll Source
   var setArcticScroll = function () {
